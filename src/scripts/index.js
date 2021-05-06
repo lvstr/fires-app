19d8a15525af6a467a38af2b211078c0646bfb7d
lvstr <serankucing@gmail.com>
2021-05-07 02:58:21 +0700

    First Commit

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
+import "../styles/menu.css";
+import "../public/images/logo/fires.png";
+import "./main";
+
+console.log("Hello Coders! :)");
